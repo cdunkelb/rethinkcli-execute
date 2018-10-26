40f17c3d50f48f6ca41348c6313625f9ad9cde07
Carl Dunkelberger <carl.dunkelberger@docker.com>
2018-10-26 15:23:08 -0400

    changed example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rethinkcli-execute
 basic bash script to execute rethink queries against DTR
+
+Manually executing commands against the DTR rethinkDB carries the chance of corrupting the data. Make sure to take a backup before running any queries.
+
+Usage:
+rethink-execute.sh RETHINK_QUERY
+
+Example:
+rethink-execute.sh "r.db('dtr2').info()"
+
--- rethink-execute.sh
+++ rethink-execute.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+usage () {
+echo "Usage: $0 RETHINK_QUERY"
+echo "Example: $0 \"r.db('dtr2').info()\"" 
+}
+
+[ -z "$1" ] && usage && exit
+
 REPLICA_ID=$(docker inspect $(docker ps -q --filter name=dtr-rethinkdb) --format {{.Name}} | cut -d '-' -f3)
 echo "parsed replica id as $REPLICA_ID"
 echo "Executing: $1"
 echo "$1" | docker run --entrypoint=rethinkcli -i --rm --net dtr-ol -e DTR_REPLICA_ID=$REPLICA_ID -v dtr-ca-$REPLICA_ID:/ca docker/dtr-rethink:2.5.0 non-interactive
+echo
 echo "Done"
